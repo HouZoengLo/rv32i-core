60f2c1bd6c8ca51987dd3153ec85ad30ce3f2883
HouZoengLo <houzoenglaw@outlook.com>
2025-03-06 14:42:00 +0800

    rv32i

--- code/pipeline/inst_mem.v
+++ code/pipeline/inst_mem.v
@@ -11,20 +11,17 @@ module inst_mem (
 
     // test code
     initial begin
-        internal_reg[0]     =   32'h00000093;
-        internal_reg[1]     =   32'h30000f93;
-        internal_reg[2]     =   32'h40000f13;
-        internal_reg[3]     =   32'h004f1f13;
-        internal_reg[4]     =   32'h00000e93;
-        internal_reg[5]     =   32'h001e8e93;
-        internal_reg[6]     =   32'hffee9ee3;
-        internal_reg[7]     =   32'h00008103;
-        internal_reg[8]     =   32'h00510113;
-        internal_reg[9]     =   32'h00208023;
-        internal_reg[10]    =   32'h00108093;
-        internal_reg[11]    =   32'hfdf09ee3;
-        internal_reg[12]    =   32'h00000093;
-        internal_reg[13]    =   32'hfc000ae3;
+        internal_reg[0]     =   32'h0a800093;
+        internal_reg[1]     =   32'h00102023;
+        internal_reg[2]     =   32'h00002c03;
+        internal_reg[3]     =   32'h001c0c33;
+        internal_reg[4]     =   32'h018c0c63;
+        internal_reg[5]     =   32'h00a00513;
+        internal_reg[6]     =   32'h00b00593;
+        internal_reg[7]     =   32'h00c00613;
+        internal_reg[8]     =   32'h00d00693;
+        internal_reg[9]     =   32'h00e00713;
+        internal_reg[10]    =   32'h01f00f93;
     end
 
 endmodule
